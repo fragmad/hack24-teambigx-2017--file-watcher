a5530d795e1a185435e07e1f78dce64afbd51d21
Will Ellwood <fragmad@gmail.com>
2017-03-19 01:46:24 +0000

    This is semi useful now

--- watcher.py
+++ watcher.py
@@ -1,7 +1,25 @@
+import hashlib
 import os
+
+VERBOSE = True
 
 # Top level directories in ownCloud's data directory. 
 IGNORED_DIRECTORIES = ['files_external']
+
+# Previous version of the file list
+last_files_tree = []
+
+
+def hash_file(file_path):
+    BLOCKSIZE = 65536
+    hasher = hashlib.md5()
+    with open(file_path, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(BLOCKSIZE)
+    return hasher.hexdigest()
+
 
 def calculate_ignored(to_ignore, root):
     ignored = []
@@ -11,19 +29,32 @@ def calculate_ignored(to_ignore, root):
 
 def walk_directories(top):
     to_ignore = calculate_ignored(IGNORED_DIRECTORIES, top)
+    list_of_files = []
 
     for root, dirs, files in os.walk(top, topdown=True):
-        list_of_files = [] 
+        dict_of_files = {}
+
         for name in files:
             if root not in to_ignore:
-                print root
-                print(os.path.join(root, name))
+                path = os.path.join(root, name)
+                leaf_dict = {"path" : path, 'filename' : name, 'hash' : hash_file(path)}
+                list_of_files.append(leaf_dict)
+    return list_of_files
        # for name in dirs:
        #     print(os.path.join(root, name))
 
-def do_magic(top):
+def construct_file_tree_dicts(top):
     top = os.path.expanduser(top)
-    walk_directories(top)
+    file_list = walk_directories(top)
+
+    if VERBOSE is True:
+        for f in file_list:
+            print f['path']
+            print f['filename']
+            print f['hash']
+
+    return file_list
 
 if __name__ == '__main__':
-    do_magic("~/tmp/storage/")
+    construct_file_tree_dicts("~/tmp/storage/")
+    
